3292beb07ab55e20a72cb885db57c440f6fe1c67
superlemon57 <lpzthomas444@gmail.com>
2025-10-01 21:58:18 +0200

    added 42 header

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -4,4 +4,17 @@
 -- See the kickstart.nvim README for more information
 return {
   'xiyaowong/transparent.nvim',
+  "Diogo-ss/42-header.nvim",
+  cmd = { "Stdheader" },
+  keys = { "<F1>" },
+  opts = {
+    default_map = true, -- Default mapping <F1> in normal mode.
+    auto_update = true, -- Update header when saving.
+    user = "username", -- Your user.
+    mail = "your@email.com", -- Your mail.
+    -- add other options.
+  },
+  config = function(_, opts)
+    require("42header").setup(opts)
+  end,
 }
