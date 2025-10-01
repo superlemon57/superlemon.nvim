47b36c20c2d2d632c86f7c689b2f23deb5520aa7
superlemon57 <lpzthomas444@gmail.com>
2025-10-01 22:06:26 +0200

    fixed syntax

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,20 +3,22 @@
 --
 -- See the kickstart.nvim README for more information
 return {
-  'xiyaowong/transparent.nvim',
-  opts = {},
-  
-  "Diogo-ss/42-header.nvim",
-  cmd = { "Stdheader" },
-  keys = { "<F1>" },
-  opts = {
-    default_map = true, -- Default mapping <F1> in normal mode.
-    auto_update = true, -- Update header when saving.
-    user = "username", -- Your user.
-    mail = "your@email.com", -- Your mail.
-    -- add other options.
+  {
+    'xiyaowong/transparent.nvim',
+    opts = {},
   },
-  config = function(_, opts)
-    require("42header").setup(opts)
-  end,
+  {
+    "Diogo-ss/42-header.nvim",
+    cmd = { "Stdheader" },
+    keys = { "<F1>" },
+    opts = {
+      default_map = true, -- Default mapping <F1> in normal mode.
+      auto_update = true, -- Update header when saving.
+      user = "username", -- Your user.
+      mail = "your@email.com", -- Your mail.
+    },
+    config = function(_, opts)
+      require("42header").setup(opts)
+    end,
+  },
 }
